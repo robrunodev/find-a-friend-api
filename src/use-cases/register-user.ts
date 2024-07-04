508e71ecd4513bcde6b9f9ca0505b15ecb97016c
Rodrigo Bruno <ro_web1@hotmail.com>
2024-07-04 09:43:18 -0300

    feat: add register user use case and types

--- src/use-cases/register-user.ts
+++ src/use-cases/register-user.ts
@@ -1,4 +1,16 @@
 import { type UsersRepository } from '@/repositories/users-repository'
+import { type User } from '@prisma/client'
+
+interface RegisterUserUseCaseParams {
+  name: string
+  email: string
+  whatsapp: string
+  password: string
+}
+
+interface RegisterUserUseCaseResponse {
+  user: User
+}
 
 export class RegisterUserUseCase {
   constructor(private readonly userRepository: UsersRepository) {}
